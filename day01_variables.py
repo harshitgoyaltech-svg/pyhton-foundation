45bd5c5fb8f22890b09e279e093f03ddfecb090a
Harshit goyal <harshit.goyal.tech@gmail.com>
2026-07-25 02:07:42 +0530

    DAY 1 - VARIABLES AND INPUT OUTPUT

---
+++ day01_variables.py
@@ -0,0 +1,12 @@
+print("=== Student Information ===")
+
+name = input("Enter your name: ")
+college = input("Enter your college: ")
+branch = input("Enter your branch: ")
+year = input("Enter your year: ")
+
+print("\n----- DETAILS -----")
+print("Name :", name)
+print("College :", college)
+print("Branch :", branch)
+print("Year :", year)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Python Foundation
+
+This repository contains my Python learning journey.
+
+## Day 1
+- Variables
+- Input
+- Output
+- Print statements
+
+Goal: Build a strong Python foundation for Data Science, Railways, and Clean Energy projects.
