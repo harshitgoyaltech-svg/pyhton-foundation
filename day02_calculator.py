bb5bcbe741e092bb5b49be41f538ff9a94b59fdb
Harshit goyal <harshit.goyal.tech@gmail.com>
2026-07-26 23:01:13 +0530

    DAY 02 - DATA TYPES AND CALCULATOR

---
+++ day02_calculator.py
@@ -0,0 +1,16 @@
+name= "HARSHIT"
+age = 20
+course = "Python"
+branch = "CSE"
+print("My name is", name)
+print("My age is", age)
+print("I am studying", course)
+print("My branch is", branch)
+print("enter the variable 1: ")
+a = int(input())
+print("enter the variable 2: ")
+b = int(input())
+print("The sum of the two variables is: ", a + b)
+print("The difference of the two variables is: ", a - b)
+print("The product of the two variables is: ", a * b)
+print("The division of the two variables is: ", a / b)
